6a852539475b6b704cd4468bf81376cd51d52855
MariliisTamm <mariliis.tamm@tthkee.onmicrosoft.com>
2026-01-09 10:30:07 +0200

    kirjutasin

---
+++ konspekt/Program.cs
@@ -0,0 +1,141 @@
+﻿namespace EsimeneProjekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* näiteprogramm 1*/
+            //int palk = 2345;
+            //int kulud = 1122;
+
+            //Console.WriteLine("Kas see kuu kulusid või väljaminekuid oli? (jah/ei)");
+            //string kasutajaVastus = "";
+            //kasutajaVastus = Console.ReadLine();
+            //if (kasutajaVastus == "jah")
+            //{
+            //    Console.WriteLine("Sisesta kõik kulutused ühe arvuna");
+            //    kulud = int.Parse(Console.ReadLine());
+            //}
+
+            //int kontojääk = palk - kulud;
+            //Console.WriteLine(kontojääk + " puulehte on alles."); // print(kontojääk + " puuehte on alles")
+            //Console.WriteLine("Palun sisesta oma hästi tehtud töö boonus: ");
+            //int boonus = int.Parse(Console.ReadLine()); // boonus = int(input("Palun sisesta oma hästi tehtud töö boonus: "))
+            //kontojääk = kontojääk + boonus; // same python
+            //Console.WriteLine("Sinu lõppväljavõte on: " + kontojääk + " puulehte");
+
+            /* näiteprogramm 2 */
+            /*
+             * klient tahab et programm teeks: 
+             * programm küsib kasutajalt tema vanust
+             * kui ta on noorem kui 18 öeldakse kasutajale "alaealisi sisse ei lasta"
+             * kui on vanem kui 18, pakutakse mojitot
+             * 
+             */
+
+            /*int kasutajaVanus = 0;
+            Console.WriteLine("Kui vana sa oled?");
+            kasutajaVanus = int.Parse(Console.ReadLine());
+            if (kasutajaVanus >= 18)
+            {
+                Console.WriteLine("Tere, siin on sinu mojito 🍹");
+            }
+            else
+            {
+                Console.WriteLine("Alaealisi sisse ei lasta, mine vembutembumaale. 🤡");
+            }*/
+
+            /*
+             näiteprogramm 3:
+
+            klient tahab et programm teeks:
+             - küsib kasutajalt tema kasutajaNime
+             - küsib kasutajalt tema parooli
+             - logib kasutaja sisse kui mõlemad on õiged
+             */
+            // hint: mida teeb &&?
+
+            /* siia käib vooskeemi järgi kirjutatud kood näidisülesanne 3 kohta */
+
+            ////ctrl + k + c/u
+            ///
+            /*int mingiArv = 4;
+
+            mingiArv = mingiArv + 15; //
+            mingiArv = mingiArv - 15; //
+            mingiArv = mingiArv * 15; //
+            mingiArv = mingiArv / 15; //
+
+            mingiArv += 15; //
+            mingiArv -= 15; //
+            mingiArv *= 15; //
+            mingiArv /= 15; //
+
+            /* näiuteülesanne 4 */
+            /*Console.WriteLine("Palun sisesta oma vanus");
+            int kasutajaVanus = int.Parse(Console.ReadLine());
+            Console.WriteLine("Palun sisesta oma vanaema ´vanus");
+            int kasutajaVanaemaVanus = int.Parse(Console.ReadLine());
+            int vanuseVahe = kasutajaVanaemaVanus - kasutajaVanus;
+            Console.WriteLine(kasutajaVanaemaVanus - kasutajaVanus);
+            Console.WriteLine("Kui palju sa vanem oled?");
+            kasutajaVanus += int.Parse(Console.ReadLine());
+            Console.WriteLine("Vanuste vahe on nüüd: " + (kasutajaVanaemaVanus - kasutajaVanus));*/
+
+
+            ///*   -= L I H T A N D M E T Ü Ü B I D =-             */
+            //string tekst = "mingisugune inimloetav tekstike"; //tekst
+            //char täht = 'A';
+            //int arv = 1; //täisarv
+            //float komakohagaarv = 1.5f; //32bit
+            //double suuremkomakohagaarv = 1.5d; //64bit
+            //decimal kümnendsüsteemiskomakohagaarv = 1.5M;
+            //var x = 123; //Umbmäärase andmetüübiga ajutine muutuja
+            //var y = "ABC";
+            //bool jahvõiei = false; //kas true või false.
+
+            ///**/
+            ////string string = "abc"; //is bäd
+            //string sõne = "abc"; //is gud
+
+            /*  -= O M I S T U S O P E R A A T O R I D =-   */
+            /*// =  -üksik võrdusmärk omistab muutuja sisse võõrtuse, mida adresseeritakse muutuja enda nimega
+            int muutuja = 1;
+            // += -võrdusmärk mille ees on pluss, automaatselt liidab muutujale otsa võrdusmärgi teisel pool oleva väärtuse
+            muutuja += 2;
+            // -= -võrdusmärk mille ees on miinus, automaatselt lahutab muutujalt otsa võrdusmärgi teisel pool oleva väärtuse
+            muutuja -= 1;
+            // *= -võrdusmärk mille ees on korrutusmärk, automaatselt korrutab muutujat võrdusmärgi teisel pool oleva väärtusega
+            muutuja *= 3;
+            // /= -võrdusmärk mille ees on jagamismärk, automaatselt jagab muutujat võrdusmärgi teisel pool oleva väärtusega
+            muutuja /= 4;
+            // % = -võrdusmärk mille ees on jäägimärk, automaatselt võtab muutujalt jäägi võrdusmärgi teisel pool oleva väärtusega jagamisel
+            muutuja %= 2;
+            // ++ on kiirtehe, mis liidab muutujale 1
+            muutuja ++;
+            // -- on kiirtehe, mis lahutab muutujalt 1
+            muutuja --; */
+
+            /*näidisülesanne*/
+            Console.WriteLine("Kui kaugele oli su viimane kuulitõuge (m)?");
+            float kaugus = float.Parse(Console.ReadLine());
+            if (kaugus <= 1.00)
+            {
+                Console.WriteLine("Sa suudad kindlasti rohkem, cmon bruh");
+            }
+            else if (kaugus <= 2.00)
+            {
+                Console.WriteLine("No tuleb juba, jõuad veel!");
+            }
+            else if (kaugus <= 3.00)
+            {
+                Console.WriteLine("TUBLI kolm meetrit umbes, peaaegu, vist... not sure");
+            }
+            else
+            {
+                Console.WriteLine("Ossaraks, seda on varsti orbiidilt näha!");
+            }
+        }
+    }
+}
+
